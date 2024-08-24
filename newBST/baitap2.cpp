2711efd3795c3d9767d045cdb93ca75ba445bf0a
nguyenanhcd <anh487303@gmail.com>
2024-08-24 08:49:17 +0700

    bo sung

--- newBST/baitap2.cpp
+++ newBST/baitap2.cpp
@@ -306,51 +306,37 @@ void findPreSuc(Tree root, Tree &pre, Tree &suc, int value)
 
 node *concatenate(node *leftList, node *rightList)
 {
-    // If either of the list is empty, then return the other list
     if (leftList == nullptr)
         return rightList;
     if (rightList == nullptr)
         return leftList;
 
-    // Store the last Node of left List
     node *leftLast = leftList->left;
 
-    // Store the last Node of right List
     node *rightLast = rightList->left;
 
-    // Connect the last node of Left List with the first Node of the right List
     leftLast->right = rightList;
     rightList->left = leftLast;
 
-    // Left of first node points to the last node in the list
     leftList->left = rightLast;
 
-    // Right of last node refers to the first node of the List
     rightLast->right = leftList;
 
     return leftList;
 }
 
-// Function converts a tree to a circular Linked List and then returns the head of the Linked List
 node *bTreeToCList(Tree root)
 {
     if (root == nullptr)
         return nullptr;
 
-    // Recursively convert left and right subtrees
     node *left = bTreeToCList(root->left);
     node *right = bTreeToCList(root->right);
 
-    // Make a circular linked list of a single node (or root).
-    // To do so, make the right and left pointers of this node point to itself
     root->left = root->right = root;
-
-    // Step 1: Concatenate the left list with the list with a single node, i.e., current node
-    // Step 2: Concatenate the returned list with the right List
     return concatenate(concatenate(left, root), right);
 }
 
-// Display Circular Linked List
 void displayCList(node *head)
 {
     cout << "Circular Linked List is :\n";
@@ -482,9 +468,9 @@ void handleMenuChoice(Tree &t, int choice)
             cout << "No Successor" << endl;
         }
     }
-    case 9: 
+    case 9:
     {
-        node* head = bTreeToCList(t);
+        node *head = bTreeToCList(t);
         displayCList(head);
     }
     case 0:
