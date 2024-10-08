80e4baa82e6a40fb35d4f30465205d18d50e27f0
anothernguyennanhcd <hoanganh0705052706@gmail.com>
2024-10-08 15:22:03 +0700

    adđ some images

--- sort/bubleSort.cpp
+++ sort/bubleSort.cpp
@@ -3,13 +3,13 @@ using namespace std;
 
 void bubbleSort(int arr[], int n)
 {
-    for (int i = 0; i < n; i++)
+    for ( int i =0;i < n;i ++)
     {
-        for (int j = 0; j < n - i - 1; j++)
+        for ( int j = 0 ;j < n - i - 1; j ++)
         {
-            if (arr[j] > arr[j + 1])
+            if(arr[j  ] > arr[j+1])
             {
-                swap(arr[j], arr[j + 1]);
+                swap(arr[j], arr[j+1]);
             }
         }
     }
--- sort/heapSort.cpp
+++ sort/heapSort.cpp
@@ -9,6 +9,9 @@ void swap(int &a, int &b)
     b = temp;
 }
 
+// đảm bảo rằng thằng cha sẽ luôn lớn hơn thằng con
+// ban đầu biến largest dùng để lưu vị trí của thằng có giá trị lớn nhất 
+// sau khi tìm được vị trí có giá trị lớn nhất, chúng ta sẽ hoán vị giá trị của vị trí đó với largest cũ
 void heapify(int arr[], int n, int i)
 {
     int largest = i;
@@ -28,6 +31,11 @@ void heapify(int arr[], int n, int i)
     }
 }
 
+
+// hàm heapsort sẽ heapìy khi 
+// thực hiện heapify ở các phần  là lá
+// sau đó nó sẽ hoán đổi phần tử đầu tiên với phần tử có vị trí cuối cùng để đẩy phần tử lớn nhất xuống vị trí cuối cùng của mảng
+// và tiếp tục heapify để tiếp tục tìm nút lớn nhất
 void heapSort(int arr[], int n)
 {
     for (int i = n / 2 - 1; i >= 0; i--)
